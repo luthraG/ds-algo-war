bf04e2322af6cd1e9c2d7a81123b4761bfe77a68
Gaurav Luthra <luthra.zenith@gmail.com>
2019-08-24 14:52:31 +0530

    August 24, problem 8

---
+++ general-practice/24_08_2019/p8.py
@@ -0,0 +1,53 @@
+'''
+    The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+    Find the sum of all the primes not greater than given N.
+
+    Input Format
+
+    The first line contains an integer T i.e. number of the test cases. 
+    The next T lines will contains an integer N.
+
+    Constraints
+
+    1 <= T <= 10^4
+    1 <= N <= 10^6
+
+    Output Format
+
+    Print the value corresponding to each test case in separate line.
+
+    Algorithm 5: Here we use sieve of rwh, along with precomputing the sums till max limit
+'''
+
+from timeit import default_timer as timer
+
+def sumOfPrimes(number):
+    sum = 2
+    number = number + 1
+    primes = [True] * number
+    # Here number denotes the max possible number
+    sumArray = [0] * (number + 1)
+    sumArray[2] = 2
+
+    limit = int(number ** 0.5) + 1
+    for i in range(3, limit, 2):
+        if primes[i]:
+            primes[i*i::2*i] = [False] * ((number - i * i - 1)//(2 * i) + 1)
+        
+    for i in range(3, number, 2):
+        if primes[i]:
+            sum += i
+        sumArray[i] = sum
+        sumArray[i + 1] = sum
+
+    return sumArray
+
+if __name__ == '__main__':
+    number = int(input('Enter the number :: '))
+    max_number = 1000000
+    start = timer()
+    sumArray = sumOfPrimes(max_number)
+    print('Sum of primes not greater than {} is {}'.format(number, sumArray[number]))
+    end = timer()
+    print('Time taken is : {}'.format(end - start))
