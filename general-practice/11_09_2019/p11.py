1ea69c7f59cf8c29efc53f22b161da5d98182b97
Gaurav Luthra <luthra.zenith@gmail.com>
2019-09-11 18:30:30 +0530

    Optimize algorithm by at least 10 times

--- general-practice/11_09_2019/p11.py
+++ general-practice/11_09_2019/p11.py
@@ -11,8 +11,15 @@ start = timer()
 sum_of_digits = 0
 
 number = 2 << (power - 1)
-while number != 0:
-    sum_of_digits += (number % 10)
-    number //= 10
+number = str(number)
+length = len(number)
+i = 0
+while i < length:
+    sum_of_digits += int(number[i])
+    i += 1
+# while number != 0:
+#     sum_of_digits += (number % 10)
+#     number //= 10
 end = timer()
 print('Sum of digits of 2 raised to power {} is {}'.format(power, sum_of_digits))
+print('Time taken is {}'.format(end - start))
