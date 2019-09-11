f281816f5dcc86a9f4ae97ff8ee54e95678e1ce3
Gaurav Luthra <luthra.zenith@gmail.com>
2019-09-11 18:26:09 +0530

    Sum of digits of 2 raised to N

---
+++ general-practice/11_09_2019/p11.py
@@ -0,0 +1,18 @@
+'''
+    2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+    What is the sum of the digits of the number 2^1000?
+'''
+
+from timeit import default_timer as timer
+
+power = int(input('Enter the power that needs to be raised to base 2 :: '))
+start = timer()
+sum_of_digits = 0
+
+number = 2 << (power - 1)
+while number != 0:
+    sum_of_digits += (number % 10)
+    number //= 10
+end = timer()
+print('Sum of digits of 2 raised to power {} is {}'.format(power, sum_of_digits))
